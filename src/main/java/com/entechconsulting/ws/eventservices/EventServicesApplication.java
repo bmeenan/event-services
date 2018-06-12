17766dbe874c334775e6cf4e3dcc8bce8b3ef22a
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-12 14:27:00 -0400

    More code for db work

--- src/main/java/com/entechconsulting/ws/eventservices/EventServicesApplication.java
+++ src/main/java/com/entechconsulting/ws/eventservices/EventServicesApplication.java
@@ -2,11 +2,17 @@ package com.entechconsulting.ws.eventservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+import java.text.*;
+
 
 @SpringBootApplication
 public class EventServicesApplication {
 
 	public static void main(String[] args) {
+		Date date = new Date();
+		SimpleDateFormat ft = new SimpleDateFormat("MM/dd/YYYY");
+		System.out.println(ft.format(date));
 		SpringApplication.run(EventServicesApplication.class, args);
 	}
 }
---
+++ src/main/java/com/entechconsulting/ws/eventservices/MainController.java
@@ -0,0 +1,20 @@
+package com.entechconsulting.ws.eventservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.entechconsulting.ws.eventservices.Event;
+//import com.entechconsulting.ws.eventservices.EventRepository;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController{
+  @GetMapping(path="/all")
+  public @ResponseBody String getAllEvents(){
+    return "Hello there";
+  }
+}
