8e3d2bfa7b62563f451f5ebaca380bdae2dcecf4
bmeenan <brian.meenan@entechconsulting.com>
2018-06-18 13:55:17 -0400

    initial stub of a test for event controller. also added junit and mockit dependencies with pom.xml

---
+++ src/test/java/com/entechconsulting/eventservices/controller/EventControllerTest.java
@@ -0,0 +1,11 @@
+package com.entechconsulting.eventservices.controller;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+@Ignore
+public class EventControllerTest {
+
+}
