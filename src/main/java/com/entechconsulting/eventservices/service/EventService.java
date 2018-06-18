5d5dbe021acf4f43a48964b7d935f1cfb6dfceef
psavella <patrick.savella@entechconsulting.com>
2018-06-18 11:24:11 -0400

    added TempEventDTO Class edited TempEvent Class added saveTempEvent and toTempEvent in EventServices Class

--- src/main/java/com/entechconsulting/eventservices/service/EventService.java
+++ src/main/java/com/entechconsulting/eventservices/service/EventService.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.entechconsulting.eventservices.dto.TempEventDTO;
+import com.entechconsulting.eventservices.repository.TempEvent;
+import com.entechconsulting.eventservices.repository.TempEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,7 @@ import com.entechconsulting.eventservices.repository.MotionEventRepository;
 public class EventService {
 	@Autowired
 	private MotionEventRepository motionEventRepository;
+	private TempEventRepository tempEventRepository;
 
 	public void saveMotionEvent(MotionEventDTO dto) {
 		//save motion event repository object
@@ -35,5 +39,26 @@ public class EventService {
 		motionEvent.setReceivedTs(formatter.format(date));
 
 		return motionEvent;
+	}
+
+	public void saveTempEvent(TempEventDTO dto) {
+		//save motion event repository object
+		TempEvent tempEvent = toTempEvent(dto);
+		tempEventRepository.save(tempEvent);
+	}
+	private TempEvent toTempEvent(TempEventDTO dto){
+		TempEvent tempEvent = new TempEvent();
+
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+		Date date = new Date();
+
+		tempEvent.setOccurredTs(dto.getOccurred_ts().split("\\.")[0]);
+		tempEvent.setRawData(dto.toString());
+		tempEvent.setSensorId(dto.getSensor_id());
+		tempEvent.setReceivedTs(formatter.format(date));
+		tempEvent.setTemp(dto.getTemp());
+		tempEvent.setHumidity(dto.getHumidity());
+
+		return tempEvent;
 	}
 }
