d8615852f79a68f6e9a0cd351adee78e3f719328
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-19 11:27:24 -0400

    added 100% test coverage to event controller

--- src/test/java/com/entechconsulting/eventservices/controller/EventControllerTest.java
+++ src/test/java/com/entechconsulting/eventservices/controller/EventControllerTest.java
@@ -1,5 +1,7 @@
 package com.entechconsulting.eventservices.controller;
 
+import com.entechconsulting.eventservices.repository.MotionEvent;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,6 +11,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.entechconsulting.eventservices.EventTestUtils;
 import com.entechconsulting.eventservices.dto.MotionEventDTO;
+import com.entechconsulting.eventservices.repository.MotionEventRepository;
 import com.entechconsulting.eventservices.repository.TempEventRepository;
 import com.entechconsulting.eventservices.service.EventService;
 
@@ -19,6 +22,8 @@ public class EventControllerTest {
 	EventService service;
 	@Mock
 	TempEventRepository tempEventRepository;
+	@Mock
+	MotionEventRepository motionEventRepository;
 	@InjectMocks
 	EventController controller;
 	
@@ -28,11 +33,31 @@ public class EventControllerTest {
 		controller.addMotion(dto);
 		Mockito.verify(service).saveMotionEvent(dto);
 	}
+
+    @Test
+    public void getImageById_invokesService_ReturnsByteArray(){
+        Integer id = 181;
+        MotionEvent motionEvent = EventTestUtils.testMotionEvent();
+        Mockito.when(service.getImgById(id)).thenReturn(motionEvent);
+        controller.getImageById(id);
+        //Mockito.verify(motionEvent).getImg();
+
+    }
 	
 	@Test
 	public void getTempEvents_invokesTempRepo_andReturnsIterable() {
 		controller.getTempEvents();
 		Mockito.verify(tempEventRepository).findAll();
+
 	}
+	
+	@Test 
+	public void getMotionEvents_invokesMotionRepo_andReturnsIterable() {
+		controller.getMotionEvents();
+		Mockito.verify(motionEventRepository).findAllEvent();
+	}
+
+
+
 
 }
