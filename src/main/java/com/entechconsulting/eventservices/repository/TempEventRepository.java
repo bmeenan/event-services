1714e24e0cbec54c4bf3b770d850f763db23c2f3
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-20 12:54:48 -0400

    fixed issue with query for temp data

--- src/main/java/com/entechconsulting/eventservices/repository/TempEventRepository.java
+++ src/main/java/com/entechconsulting/eventservices/repository/TempEventRepository.java
@@ -3,15 +3,18 @@ package com.entechconsulting.eventservices.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import com.entechconsulting.eventservices.repository.TempEvent;
 import org.springframework.data.repository.query.Param;
 
 public interface TempEventRepository extends CrudRepository<TempEvent,Long>{
     Iterable<TempEvent> findAll();
 
 
+//    //fix query
+//    @Query("select new com.entechconsulting.eventservices.repository.TempEvent(t.temp, t.humidity) from TempEvent t where t.occurred_ts < futureDate AND t.occurred_ts >  pastDate")
+//    Iterable<TempEvent> getTempHumidity(@Param("pastDate") String pastDate,@Param("futureDate") String futureDate);
+
     //fix query
-    @Query("select new com.entechconsulting.eventservices.repository.TempEvent(t.temp, t.humidity) from TempEvent t where t.occurred_ts < futureDate AND t.occurred_ts >  pastDate")
+    @Query("select new com.entechconsulting.eventservices.repository.TempEvent(t.temp, t.humidity) from TempEvent t where t.occurred_ts < :futureDate AND t.occurred_ts > :pastDate")
     Iterable<TempEvent> getTempHumidity(@Param("pastDate") String pastDate,@Param("futureDate") String futureDate);
 
 }
