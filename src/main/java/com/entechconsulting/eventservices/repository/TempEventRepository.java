474b48c778020e820cc72288d31ab56d4b096c9d
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-21 09:19:29 -0400

    removed unnecessary comment

--- src/main/java/com/entechconsulting/eventservices/repository/TempEventRepository.java
+++ src/main/java/com/entechconsulting/eventservices/repository/TempEventRepository.java
@@ -7,13 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface TempEventRepository extends CrudRepository<TempEvent,Long>{
     Iterable<TempEvent> findAll();
-
-
-//    //fix query
-//    @Query("select new com.entechconsulting.eventservices.repository.TempEvent(t.temp, t.humidity) from TempEvent t where t.occurred_ts < futureDate AND t.occurred_ts >  pastDate")
-//    Iterable<TempEvent> getTempHumidity(@Param("pastDate") String pastDate,@Param("futureDate") String futureDate);
-
-    //fix query
+    
     @Query("select new com.entechconsulting.eventservices.repository.TempEvent(t.temp, t.humidity) from TempEvent t where t.occurred_ts < :futureDate AND t.occurred_ts > :pastDate")
     Iterable<TempEvent> getTempHumidity(@Param("pastDate") String pastDate,@Param("futureDate") String futureDate);
 
