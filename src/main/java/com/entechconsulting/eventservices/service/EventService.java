5ab4524a7e3d0b47d707a4d237a3baed8e06225a
bmeenan <brian.meenan@entechconsulting.com>
2018-07-06 10:03:51 -0400

    organized event services service class - organized imports

--- src/main/java/com/entechconsulting/eventservices/service/EventService.java
+++ src/main/java/com/entechconsulting/eventservices/service/EventService.java
@@ -1,5 +1,16 @@
 package com.entechconsulting.eventservices.service;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.TimeZone;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.entechconsulting.eventservices.controller.EventController;
 import com.entechconsulting.eventservices.dto.MotionEventDTO;
 import com.entechconsulting.eventservices.dto.TempEventDTO;
@@ -8,18 +19,6 @@ import com.entechconsulting.eventservices.repository.MotionEventRepository;
 import com.entechconsulting.eventservices.repository.TempEvent;
 import com.entechconsulting.eventservices.repository.TempEventRepository;
 import com.entechconsulting.eventservices.utilities.CompressionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Objects;
-import java.util.TimeZone;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 @Service
 public class EventService {
@@ -30,20 +29,21 @@ public class EventService {
 
 	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(EventController.class);
 
-	//public method that changes DTO to motion event object, then calls the motion event repo
-	public void saveMotionEvent(MotionEventDTO dto){
-		//save motion event repository object
+	// public method that changes DTO to motion event object, then calls the motion
+	// event repo
+	public void saveMotionEvent(MotionEventDTO dto) {
+		// save motion event repository object
 		MotionEvent motionEvent = toMotionEvent(dto);
 		LOGGER.info("Saving motion event...");
 		motionEventRepository.save(motionEvent);
 		LOGGER.info("Motion Event saved");
 	}
 
-	//converts dto to motion event object
-	private MotionEvent toMotionEvent(MotionEventDTO dto){
+	// converts dto to motion event object
+	private MotionEvent toMotionEvent(MotionEventDTO dto) {
 		MotionEvent motionEvent = new MotionEvent();
 
-		//sets appropriate date format
+		// sets appropriate date format
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		formatter.setTimeZone(TimeZone.getDefault());
 
@@ -61,20 +61,21 @@ public class EventService {
 		return motionEvent;
 	}
 
-	//public method that changes DTO to temp event object, then calls the temp event repo
+	// public method that changes DTO to temp event object, then calls the temp
+	// event repo
 	public void saveTempEvent(TempEventDTO dto) {
-		//save temp event repository object
+		// save temp event repository object
 		TempEvent tempEvent = toTempEvent(dto);
 		LOGGER.info("Saving Temp Event...");
 		tempEventRepository.save(tempEvent);
 		LOGGER.info("Temp Event saved");
 	}
 
-	//converts dto to temp event object
+	// converts dto to temp event object
 	private TempEvent toTempEvent(TempEventDTO dto) {
 		TempEvent tempEvent = new TempEvent();
 
-		//appropriate date settings
+		// appropriate date settings
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		formatter.setTimeZone(TimeZone.getDefault());
 		Date date = new Date();
@@ -89,33 +90,35 @@ public class EventService {
 		return tempEvent;
 	}
 
-	//passed id and calls the motionEventRepo
-	public MotionEvent getImgById(Integer id){
+	// passed id and calls the motionEventRepo
+	public MotionEvent getImgById(Integer id) {
 		LOGGER.info("Returning image from Database");
 		return motionEventRepository.findImgById(id).iterator().next();
 	}
 
-	//gets occurred date, sets boundaries, and then calls temp event repo to get teh possible temp events
-	public Iterable<TempEvent> getTempByDate(String date){
-        int milliseconds_for_change =900000;
+	// gets occurred date, sets boundaries, and then calls temp event repo to get
+	// teh possible temp events
+	public Iterable<TempEvent> getTempByDate(String date) {
+		int milliseconds_for_change = 900000;
 
-		//do date formatting here
+		// do date formatting here
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		formatter.setTimeZone(TimeZone.getDefault());
 
-        Date pastDate;
-        Date futureDate;
-        try {
-            pastDate = new Date(formatter.parse(date).getTime() - milliseconds_for_change);
-            futureDate = new Date(formatter.parse(date).getTime() + milliseconds_for_change);
+		Date pastDate;
+		Date futureDate;
+		try {
+			pastDate = new Date(formatter.parse(date).getTime() - milliseconds_for_change);
+			futureDate = new Date(formatter.parse(date).getTime() + milliseconds_for_change);
 
-			LOGGER.info("Returning Temp events from " + formatter.format(pastDate) + " to " + formatter.format( futureDate));
-            return tempEventRepository.getTempHumidity(formatter.format(pastDate),formatter.format( futureDate));
+			LOGGER.info(
+					"Returning Temp events from " + formatter.format(pastDate) + " to " + formatter.format(futureDate));
+			return tempEventRepository.getTempHumidity(formatter.format(pastDate), formatter.format(futureDate));
 
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 //adding comment
